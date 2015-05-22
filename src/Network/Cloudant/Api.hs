5dd1e587a950403f33b3dcb1f747877d4d244734
Owain Lewis <owain@owainlewis.com>
2015-05-22 22:33:12 +0100

    update params

--- src/Network/Cloudant/Api.hs
+++ src/Network/Cloudant/Api.hs
@@ -2,6 +2,7 @@
 module Network.Cloudant.Api where
 
 import           Control.Lens
+import qualified Data.ByteString.Char8      as BS
 import qualified Data.ByteString.Lazy.Char8 as L
 import           Data.Monoid                (mconcat, (<>))
 import           Network.Wreq
@@ -22,16 +23,16 @@ class Cloudant a where
     resourceEndpoint :: a -> String -> String
 
 -- Simple GET requests
-getWithAuth :: IO L.ByteString
-getWithAuth = do
-    let opts = defaults & auth ?~ basicAuth "myuser" "mypass"
-    r <- getWith opts "http://httpbin.org/get"
+getWithAuth :: String -> String -> String -> IO L.ByteString
+getWithAuth url user pass = do
+    let opts = defaults & auth ?~ basicAuth (BS.pack user) (BS.pack pass)
+    r <- getWith opts url
     return $ (r ^. responseBody)
 
 -- Types
 
-data GetDatabases = GetDatabases {
-} deriving ( Show )
+-- List all databases for an account
+data GetDatabases = GetDatabases deriving ( Show )
 
 instance Cloudant GetDatabases where
-    resourceEndpoint _ account = getHTTPEndpoint account "/all_dbs"
+    resourceEndpoint _ account = getHTTPEndpoint account "/_all_dbs"
