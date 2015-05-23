c8caa50e825bbf0b6d24b79424b0bf7919d4f6ab
Owain Lewis <owain@owainlewis.com>
2015-05-23 19:19:45 +0100

    sketching out the API

--- src/Network/Cloudant/Api.hs
+++ src/Network/Cloudant/Api.hs
@@ -1,17 +1,64 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Network.Cloudant.Api
-  ( getWithAuth )
+  ( )
   where
 
 import           Control.Lens
 import           Data.Aeson
 import qualified Data.ByteString.Char8      as BS
-import qualified Data.ByteString.Lazy.Char8 as L
--- import qualified Data.Map                   as M
+import qualified Data.ByteString.Lazy.Char8 as LBS
+import           Data.Maybe                 (fromJust, fromMaybe)
 import           Data.Monoid                (mconcat, (<>))
-import           Network.Wreq
+import           Network.HTTP.Conduit
 
--- Resource endpoints
+-- Basic authentication tuple
+type Auth = (String, String)
+
+user :: Auth -> String
+user auth = auth ^. _1
+
+pass :: Auth -> String
+pass auth = auth ^. _2
+
+-- Build a request with basic authentication
+--
+buildRequest ::
+    String ->
+    String ->
+    Auth ->
+    Maybe BS.ByteString ->
+    IO Request
+buildRequest reqMethod url auth body = do
+    let reqBody  = fromMaybe (BS.pack "") body
+        uri = applyBasicAuth (BS.pack $ user auth) (BS.pack $ pass auth) $ fromJust $ parseUrl url
+        request  = uri { method = (BS.pack reqMethod)
+                       , secure = True
+                       , port = 443
+                       }
+    return request
+
+-- Run a HTTP request returning the response body
+--
+runRequest :: IO Request -> IO LBS.ByteString
+runRequest request = do
+    req <- request
+    response <- withManager $ httpLbs req
+    return . responseBody $ response
+
+-- Make a HTTP request
+-- HTTP method, url, basic authentication and an optional request body
+--
+-- Example :
+--     makeRequest "https://httpbin.org/get" ("jack", "secret") Nothing
+--
+makeRequest ::
+    String -> -- HTTP Method e.g GET
+    String ->
+    Auth   ->
+    Maybe BS.ByteString ->
+    IO LBS.ByteString
+makeRequest method url auth body =
+    runRequest $ buildRequest method url auth body
 
 class Cloudant a where
     getResource :: a -> String
@@ -20,52 +67,43 @@ urlForAccount :: String -> String
 urlForAccount account = "https://" <> account <> ".cloudant.com"
 
 getHTTPEndpoint :: String -> String -> String
-getHTTPEndpoint account resource = mconcat [ "https://", account, ".cloudant.com", resource ]
+getHTTPEndpoint account resource =
+    mconcat [ "https://"
+            , account
+            , ".cloudant.com"
+            , resource ]
 
-buildAuth user pass = defaults & auth ?~ basicAuth (BS.pack user) (BS.pack pass)
+-- Requests
+-- *****************************************************
 
-getWithAuth :: String -> String -> String -> IO L.ByteString
-getWithAuth url user pass = do
-    let opts = defaults & auth ?~ basicAuth (BS.pack user) (BS.pack pass)
-    r <- getWith opts url
-    return $ (r ^. responseBody)
-
-postWithAuth :: ToJSON a =>  String -> String -> String -> a -> IO L.ByteString
-postWithAuth url user pass body = do
-    let opts = buildAuth user pass
-    r <- postWith opts url (toJSON body)
-    return $ (r ^. responseBody)
-
-runRequest :: Cloudant a => a -> String -> String -> IO L.ByteString
-runRequest request user password =
-  getWithAuth (getResource request) user password
-
--- Get permissions for a database
---
 data GetPermissions = GetPermissions {
-    account  :: String
-  , database :: String
+    getPermissionsAccount  :: String
+  , getPermissionsDatabase :: String
 }
 instance Cloudant GetPermissions where
     getResource x = getHTTPEndpoint userAccount resource
-                    where userAccount = (account x)
-                          resource    = "/_api/v2/db/" <> (database x) <> "/_security"
+                    where userAccount = (getPermissionsAccount x)
+                          resource    = "/_api/v2/db/" <> (getPermissionsDatabase x) <> "/_security"
 
 instance Show GetPermissions where show = getResource
 
 -- Databases
 -- 1. Create database
--- 2. Read database
--- 3. Get databases
--- 4. Get documents
--- 5. Get changes
--- 6. Delete
-
-data GetDatabases = GetDatabases {
-
-}
-
 data CreateDatabase = CreateDatabase {
     createDatabaseAccount  :: String
   , createDatabaseDatabase :: String
 }
+-- 2. Read database
+-- 3. Get databases
+data GetDatabases = GetDatabases { getDatabasesAccount :: String }
+
+instance Cloudant GetDatabases where
+    getResource x = getHTTPEndpoint userAccount "/_all_dbs"
+        where userAccount = (getDatabasesAccount x)
+
+getDatabases :: String -> Auth -> IO LBS.ByteString
+getDatabases account auth =
+    makeRequest "GET" (getResource $ GetDatabases account) auth Nothing
+-- 4. Get documents
+-- 5. Get changes
+-- 6. Delete
--- src/Network/Cloudant/Request.hs
+++ src/Network/Cloudant/Request.hs
@@ -1,26 +1,66 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Network.Cloudant.Request where
+module Network.Cloudant.Request
+  ( Auth
+  , buildRequest
+  , runRequest
+  , makeRequest
+  ) where
 
+import           Control.Lens
 import qualified Data.ByteString.Char8      as BS
 import qualified Data.ByteString.Lazy.Char8 as LBS
 import           Data.Maybe                 (fromJust, fromMaybe)
 import           Network.HTTP.Conduit
 
-doHTTPSRequest ::
-  String ->  -- method i.e GET
-  String ->  -- HTTPS endpoint
-  String ->  -- Basic auth user
-  String ->  -- Basic auth password
-  Maybe BS.ByteString ->
-  IO (LBS.ByteString)
-doHTTPSRequest reqMethod url user pass body = do
-  let reqBody  = fromMaybe (BS.pack "") body
-      uri = applyBasicAuth (BS.pack user) (BS.pack pass) $ fromJust $ parseUrl url
-      request  = uri { method = (BS.pack reqMethod)
-                     , secure = True
-                     , port = 443
-                     }
-  r <- withManager $ httpLbs request
-  return (responseBody r)
+-- Basic authentication tuple
+type Auth = (String, String)
 
--- testRequest = doHTTPSRequest "GET" "https://httpbin.org/get" Nothing
+user :: Auth -> String
+user auth = auth ^. _1
+
+pass :: Auth -> String
+pass auth = auth ^. _2
+
+-- Build a request with basic authentication
+--
+-- Example:
+--
+--     buildRequest "GET" "https://httpbin.org/get" ("", "") Nothing
+--
+buildRequest ::
+    String ->
+    String ->
+    Auth ->
+    Maybe BS.ByteString ->
+    IO Request
+buildRequest reqMethod url auth body = do
+    let reqBody  = fromMaybe (BS.pack "") body
+        uri = applyBasicAuth (BS.pack $ user auth) (BS.pack $ pass auth) $ fromJust $ parseUrl url
+        request  = uri { method = (BS.pack reqMethod)
+                       , secure = True
+                       , port = 443
+                       }
+    return request
+
+-- Run a HTTP request returning the response body
+--
+runRequest :: IO Request -> IO LBS.ByteString
+runRequest request = do
+    req <- request
+    response <- withManager $ httpLbs req
+    return . responseBody $ response
+
+-- Make a HTTP request
+-- HTTP method, url, basic authentication and an optional request body
+--
+-- Example :
+--     makeRequest "https://httpbin.org/get" ("jack", "secret") Nothing
+--
+makeRequest ::
+    String -> -- HTTP Method e.g GET
+    String ->
+    Auth   ->
+    Maybe BS.ByteString ->
+    IO LBS.ByteString
+makeRequest method url auth body =
+    runRequest $ buildRequest method url auth body
