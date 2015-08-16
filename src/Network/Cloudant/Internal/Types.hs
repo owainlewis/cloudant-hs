aa1d457f3639f75aac42b4148cd9d69d83d9ffac
Owain Lewis <owain@owainlewis.com>
2015-08-16 19:00:08 +0100

    adds document type

--- src/Network/Cloudant/Internal/Types.hs
+++ src/Network/Cloudant/Internal/Types.hs
@@ -4,14 +4,24 @@ module Network.Cloudant.Internal.Types
   , Config(..)
   ) where
 
--- Base types
-
+-- An API key used for authentication
+-- This is made up of a username and password which get sent via basic auth
 data ApiKey = ApiKey {
     username :: String
   , password :: String
 } deriving ( Show )
 
+-- Cloudant API configuration
+--
 data Config = Config {
     url    :: String
   , apiKey :: ApiKey
 } deriving ( Show )
+
+-- A Cloudant document contains an id, revision and attributes
+--
+data Document a = Document {
+    id      :: String
+  , rev     :: String
+  , content :: a
+} deriving ( Show, Eq )
