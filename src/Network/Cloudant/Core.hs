5b6efaf8be22dcd906cd70ffd845697d504f7072
Owain Lewis <owain@owainlewis.com>
2015-08-16 18:31:08 +0100

    type signature

--- src/Network/Cloudant/Core.hs
+++ src/Network/Cloudant/Core.hs
@@ -1,5 +1,6 @@
 module Network.Cloudant.Core where
 
+import           Data.Aeson                        (ToJSON)
 import qualified Network.Cloudant.Database         as Database
 import qualified Network.Cloudant.Document         as Document
 import           Network.Cloudant.Internal.Request (runRequest)
@@ -17,6 +18,7 @@ getDatabases config = TF.transform response :: IO (Either String [String])
 
 -- Document
 
+createDocument :: ToJSON a => Config -> String -> a -> IO (Either String TF.OKResponse)
 createDocument config database document =
     TF.transform response :: IO (Either String TF.OKResponse)
     where response = runRequest config (Document.create database document)
