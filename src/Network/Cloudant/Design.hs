c788a06ac84f4741b32626d8c1274c07435f0628
Owain Lewis <owain@owainlewis.com>
2015-08-22 13:34:07 +0100

    updates design and views

--- src/Network/Cloudant/Design.hs
+++ src/Network/Cloudant/Design.hs
@@ -1,1 +1,39 @@
 module Network.Cloudant.Design where
+
+import           Network.Cloudant.Internal.Request
+import           Network.Cloudant.Util             (foldPaths)
+
+-- Instead of storing data in a document, you might also have special documents that store other content,
+-- such as functions. The special documents are called “design documents”.
+
+{-- Example design document
+    {
+        "language": "javascript",
+        "views": {
+           "all": {
+                "map": "function(doc) { emit(doc.title, doc) }",
+            },
+            "by_title": {
+                "map": "function(doc) { if (doc.title != null) emit(doc.title, doc) }",
+            }
+        },
+        "shows": {
+            "recipe": "function(doc, req) { return '<h1>' + doc.title + '</h1>' }"
+    }
+--}
+
+create :: String -> String -> RequestBuilder
+create database designDoc =
+    RequestBuilder PUT path Nothing Nothing
+        where path = foldPaths [ database
+                               , "_design"
+                               , designDoc
+                               ]
+
+get :: String -> String -> RequestBuilder
+get database designDoc =
+    RequestBuilder GET path Nothing Nothing
+        where path = foldPaths [ database
+                               , "_design"
+                               , designDoc
+                               ]
--- src/Network/Cloudant/View.hs
+++ src/Network/Cloudant/View.hs
@@ -9,4 +9,7 @@ import           Network.Cloudant.Util             (foldPaths)
 
 create :: String -> String -> BS.ByteString -> RequestBuilder
 create database designDoc view = RequestBuilder PUT path (Just view) Nothing
-    where path = foldPaths [database, "_design", designDoc]
+    where path = foldPaths [ database
+                           , "_design"
+                           , designDoc
+                           ]
