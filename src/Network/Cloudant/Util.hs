7db323ac406fac4ea04e57c8cd902d32bf73c91f
Owain Lewis <owain@owainlewis.com>
2016-01-06 13:29:10 +0000

    Update Util.hs

--- src/Network/Cloudant/Util.hs
+++ src/Network/Cloudant/Util.hs
@@ -9,12 +9,10 @@ import           Network.Cloudant.Internal.Request (withSlash)
 import           Network.Cloudant.Internal.Types
 
 -- | Generate the full IBM Cloudant URL for an account
---
 urlForAccount :: String -> String
 urlForAccount account = "https://" <> account <> ".cloudant.com"
 
 -- | Local configuration for testing against CouchDB
---
 localConfig :: Config
 localConfig = Config "http://192.168.59.103" $ ApiKey "admin" "password"
 
@@ -24,7 +22,6 @@ foldPaths :: [String] -> String
 foldPaths = mconcat . map withSlash
 
 -- | Strict encoding for Aeson
---
 strictEncode :: ToJSON a => a -> BS.ByteString
 strictEncode = LBS.toStrict . encode
 
