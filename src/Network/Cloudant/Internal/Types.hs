87c42379b151432d204a82ee2fdad555caf63c5b
Owain Lewis <owain@owainlewis.com>
2016-01-06 13:26:27 +0000

    Update Types.hs

--- src/Network/Cloudant/Internal/Types.hs
+++ src/Network/Cloudant/Internal/Types.hs
@@ -1,22 +1,23 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Network.Cloudant.Internal.Types where
+module Network.Cloudant.Internal.Types 
+    ( ApiKey(..)
+    , Config(..)
+    , Document(..)
+    ) where
 
--- An API key used for authentication
---
+-- | An API key used for authentication
 data ApiKey = ApiKey {
     username :: String
   , password :: String
 } deriving ( Show )
 
--- Cloudant API configuration
---
+-- | Cloudant API configuration
 data Config = Config {
     url    :: String
   , apiKey :: ApiKey
 } deriving ( Show )
 
--- A Cloudant document contains an id, revision and attributes
---
+-- | A Cloudant document contains an id, revision and attributes
 data Document a = Document {
     id      :: String
   , rev     :: String
